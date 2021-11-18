818b3b7728f77f1788ad47541adda200778bf63e
bobby murelo <murelobobby99@gmail.com>
2021-11-18 17:06:11 +0200

    Add project files.

---
+++ temperature conversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace temperature_conversion
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            Console.Write("Enter Fahrenheit temperature : ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Celsius temperature is" + celsius);
+            Console.ReadLine();
+
+
+            
+            Console.Write("Enter Celcius temperature : ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            fahrenheit = (celcius * 9)/5 + 32;
+            Console.WriteLine("Celsius temperature is" + celsius);
+            Console.ReadLine();
+
+            if (celcius > fahrenheit)
+                Console.WriteLine("Celcius is bigger");
+            else
+                Console.WriteLine("fahrenheit is bigger");
+        }
+    }
+
+}
